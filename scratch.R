32fa83c9d110c4ba9ede72d005acb98c0c9b50df
E.R. Crema <enrico.crema@gmail.com>
2022-02-12 08:19:12 +0000

    Test result on measurement error model

--- scratch.R
+++ scratch.R
@@ -84,13 +84,9 @@ d = list(X=round(apply(x1[,-1],1,median)),s=apply(x1[,-1],1,function(x){abs(diff
 inits = list(r1=0.01,r2=0.01,mu=4500,theta=d$X)
 constants = list(N=nrow(x1),start=5000,end=3001)
 modelCode  <- nimbleModel(code=model,constants=constants,inits=inits)
-mcmc.samples<- nimbleMCMC(code = model,constants = constants,data = d,niter = 10000, nchains = 2, thin=1, nburnin = 2000, monitors=c('r1','r2','mu','theta'), inits=inits, samplesAsCodaMCMC=TRUE,setSeed=c(123,456))
-
-
-
-
-
-
+mcmc.samples<- nimbleMCMC(code = model,constants = constants,data = d,niter = 10000, nchains = 2, thin=1, nburnin = 2000, monitors=c('r1','r2','mu'), inits=inits, samplesAsCodaMCMC=TRUE,setSeed=c(123,456))
+mcmc.samples <- do.call(rbind,mcmc.samples)
+save(mcmc.samples,file='temp.RData')
 
 
 
