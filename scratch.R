8ecc6b610004e25df8fcf52293e8934853a4e093
Enrico R. Crema <enrico.crema@gmail.com>
2022-08-08 16:30:12 +0100

    Increase number of examples and added sample figures

--- scratch.R
+++ scratch.R
@@ -8,67 +8,104 @@ source(here('src','mcsim.R'))
 source(here('src','ribbon.R'))
 source(here('src','unif2.R'))
 
-
-x = replicate(n=1000, rDoubleExponentialGrowth(a=5000,b=3001,r1=0.002,r2=-0.002,mu=4300))
+set.seed(123)
+nsample = 1000
+x = replicate(n=nsample, rDoubleExponentialGrowth(a=5000,b=3001,r1=0.002,r2=-0.002,mu=4300))
 
 # abutting, perfect info
 phases1 <- data.frame(phasename=letters[1:7])
-phases1$starts = c(5000,4700,4500,4000,3600,3500,3400)
-phases1$ends   = c(4701,4501,4001,3601,3501,3401,3001)
+phases1$starts = c(5000,4700,4500,3800,3600,3500,3400)
+phases1$ends   = c(4701,4501,3801,3601,3501,3401,3001)
 x1  <- time2phase(x,phases1)
-# phase uncertainty
+
+# abutting, regular chronology of 200 years
 phases2 <- data.frame(phasename=letters[1:10])
-phases2$starts = c(5000,4700,4700,4700,4500,4000,4000,3500,3300,3500)
-phases2$ends   = c(4201,4501,4101,4001,4001,3501,3001,3300,3001,3001)
+phases2$starts = seq(5000,3200,-200)
+phases2$ends   = seq(4801,3001,-200)
 x2  <- time2phase(x,phases2)
+
+# phase uncertainty
+phases3 <- data.frame(phasename=letters[1:14])
+phases3$starts = c(5000,5000,4700,4700,4700,4700,4700,4500,4500,4500,4000,4000,3800,3500)
+phases3$ends   = c(4001,4701,4501,4001,3801,3501,3001,4001,3801,3001,3801,3001,3001,3001)
+x3  <- time2phase(x,phases3)
+
 # true time-span
-x3  <- randtimespan(x,seed=123,limMin=10,limMax=200,edge=FALSE)
+x4  <- randtimespan(x,seed=123,limMin=10,limMax=300,edge=FALSE)
 
 
-par(mfrow=c(1,3))
-timeRange = c(5000,3001)
+
+# Aoristic Sum ----
+
+pdf(width=7,height=7,file=here('figures','figure1.pdf')) 
+par(mfrow=c(2,2))
 resolution = 50
+tt  <- 5000:3001
+dd  <- dDoubleExponentialGrowth(x=tt,a=5000,b=3001,r1=0.002,r2=-0.002,mu=4300,log=FALSE)*resolution*nsample
+timeRange = c(5000,3001)
 res1 = aoristicsum(x=x1,timeRange=timeRange,blockSize=resolution)
-plot(apply(res1[,-3],1,median),res1[,3],xlim=timeRange,type='b',pch=20,xlab='calBP',ylab='Aoristic Sum')
-abline(v=4300,lty=2,col=2)
+plot(apply(res1[,-3],1,median),res1[,3],xlim=timeRange,type='b',pch=20,xlab='calBP',ylab='Aoristic Sum',ylim=c(0,max(dd)),main='a')
+# abline(v=4300,lty=2,col=2)
+lines(tt,dd,col='steelblue',lty=2,lwd=2)
 
 res2 = aoristicsum(x=x2,timeRange=timeRange,blockSize=resolution)
-plot(apply(res2[,-3],1,median),res2[,3],xlim=timeRange,type='b',pch=20,xlab='calBP',ylab='Aoristic Sum')
-abline(v=4300,lty=2,col=2)
-
+plot(apply(res2[,-3],1,median),res2[,3],xlim=timeRange,type='b',pch=20,xlab='calBP',ylab='Aoristic Sum',ylim=c(0,max(dd)),main='b')
+# abline(v=4300,lty=2,col=2)
+lines(tt,dd,col='steelblue',lty=2,lwd=2)
 
 res3 = aoristicsum(x=x3,timeRange=timeRange,blockSize=resolution)
-plot(apply(res3[,-3],1,median),res3[,3],xlim=timeRange,type='b',pch=20,xlab='calBP',ylab='Aoristic Sum')
-abline(v=4300,lty=2,col=2)
+plot(apply(res3[,-3],1,median),res3[,3],xlim=timeRange,type='b',pch=20,xlab='calBP',ylab='Aoristic Sum',ylim=c(0,max(dd)),main='c')
+# abline(v=4300,lty=2,col=2)
+lines(tt,dd,col='steelblue',lty=2,lwd=2)
+
+res4 = aoristicsum(x=x4,timeRange=timeRange,blockSize=resolution)
+plot(apply(res4[,-3],1,median),res4[,3],xlim=timeRange,type='b',pch=20,xlab='calBP',ylab='Aoristic Sum',ylim=c(0,max(dd)),main='d')
+# abline(v=4300,lty=2,col=2)
+lines(tt,dd,col='steelblue',lty=2,lwd=2)
+legend('topright',legend=c('Aoristic Sum','True Density'),lty=c(1,2),col=c(1,'steelblue'),pch=c(20,NA),lwd=c(1,2))
+dev.off()
 
 
 
+# Monte Carlo Simulation ----
 
 res1 = mcsim(x=x1,nsim=1000,timeRange=timeRange,blockSize=resolution)
 res2 = mcsim(x=x2,nsim=1000,timeRange=timeRange,blockSize=resolution)
 res3 = mcsim(x=x3,nsim=1000,timeRange=timeRange,blockSize=resolution)
+res4 = mcsim(x=x4,nsim=1000,timeRange=timeRange,blockSize=resolution)
 
 
+pdf(width=7,height=7,file=here('figures','figure2.pdf')) 
 mdp = apply(res1[[1]],1,median)
-par(mfrow=c(1,3))
-plot(NULL,xlim=rev(range(mdp)),ylim=c(0,max(res1[[2]])))
+par(mfrow=c(2,2),lend=2)
+plot(NULL,xlim=rev(range(mdp)),ylim=c(0,max(c(res1[[2]],dd))),xlab='cal BP',main='a')
 ribbon(x=mdp,y=res1[[2]])
 lines(mdp,apply(res1[[2]],1,median),type='b',pch=20)
-abline(v=4300,lty=2,col=2)
+lines(tt,dd,col='steelblue',lty=2,lwd=2)
 
-plot(NULL,xlim=rev(range(mdp)),ylim=c(0,max(res2[[2]])))
+
+plot(NULL,xlim=rev(range(mdp)),ylim=c(0,max(c(res1[[2]],dd))),xlab='cal BP',main='b')
 ribbon(x=mdp,y=res2[[2]])
 lines(mdp,apply(res2[[2]],1,median),type='b',pch=20)
-abline(v=4300,lty=2,col=2)
+lines(tt,dd,col='steelblue',lty=2,lwd=2)
 
-plot(NULL,xlim=rev(range(mdp)),ylim=c(0,max(res3[[2]])))
+
+plot(NULL,xlim=rev(range(mdp)),ylim=c(0,max(c(res1[[2]],dd))),xlab='cal BP',main='c')
 ribbon(x=mdp,y=res3[[2]])
 lines(mdp,apply(res3[[2]],1,median),type='b',pch=20)
-abline(v=4300,lty=2,col=2)
+lines(tt,dd,col='steelblue',lty=2,lwd=2)
+
+
+plot(NULL,xlim=rev(range(mdp)),ylim=c(0,max(c(res1[[2]],dd))),xlab='cal BP',main='d')
+ribbon(x=mdp,y=res4[[2]])
+lines(mdp,apply(res4[[2]],1,median),type='b',pch=20)
+lines(tt,dd,col='steelblue',lty=2,lwd=2)
+legend('topright',legend=c('Aoristic Sum','MC Simulation','True Density'),lty=c(1,1,2),col=c(1,'lightgrey','steelblue'),pch=c(20,NA,NA),lwd=c(1,7,2),bty='n')
+dev.off()
 
 
 ### Bayesian Analyses ###
-model <- nimbleCode({
+odel <- nimbleCode({
       for (i in 1:N){
         # Growth Model Likelihood
         theta[i] ~ dDoubleExponentialGrowth(a=start,b=end,r1=r1,r2=r2,mu=mu);
