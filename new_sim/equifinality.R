6d3fad60e33c889f0e5a34e314ac2e6d1bee4ee7
Enrico R. Crema <enrico.crema@gmai.com>
2023-09-12 18:58:14 +0100

    Added equifinality experiment

--- new_sim/equifinality.R
+++ new_sim/equifinality.R
@@ -1,36 +1,37 @@
+library(here)
 library(nimbleCarbon)
+library(baorista)
+source(here('src','time2phase.R'))
+source(here('src','unifdisc.R'))
 
 
-r1  <-  0.01
-r2  <-  0.027
-a  <- dExponentialGrowth(800:301,r=r1,a=800,b=301,log=F)
-b  <- dLogisticGrowth2(800:301,r=r2,a=800,b=301,m=500,log=F)
-plot(800:301,a,type='l',xlim=c(800,301))
-lines(800:301,b,col=2)
-round(sum(a[1:300]),2)
-round(sum(b[1:300]),2)
-
-
-# Parameter Space Plot
-r = seq(0.001,0.03,length.out=1000)
-m = 801:302
-params = expand.grid(r=r,m=m)
+# Parameter Space Plot ----
+params = expand.grid(r=seq(0.001,0.03,length.out=1000),m=799:302)
 params$s <- NA
 for (i in 1:nrow(params))
 {
-print(i)
-params$s[i] <- round(sum(dLogisticGrowth2(800:500,r=params$r[i],a=800,b=301,m=params$m[i],log=F)),2)
+	params$s[i] <- round(sum(dLogisticGrowth2(800:501,r=params$r[i],a=800,b=301,m=params$m[i],log=F)),2)
 }
 
+# Subset to all runs where the time-interval of the first phase (800~501) has a cumulative probability mass of 0.13:
+
 sparam <- subset(params,s==0.13)
-plot(params$r,params$m,col='lightgrey',pch=20)
-points(sparam$r,sparam$m,col='indianred',pch=20)
+# plot(params$r,params$m,col='lightgrey',pch=20)
+# points(sparam$r,sparam$m,col='indianred',pch=20)
+rr <- sparam$r[c(1,5483,3000)]
+mm <- sparam$m[c(1,5483,3000)]
+# c1  <- dLogisticGrowth2(800:301,r=rr[1],a=800,b=301,m=mm[1],log=F)
+# c2  <- dLogisticGrowth2(800:301,r=rr[5483],a=800,b=301,m=mm[5483],log=F)
+# c3  <- dLogisticGrowth2(800:301,r=rr[3000],a=800,b=301,m=mm[3000],log=F)
+# plot(800:301,c1,type='l',xlim=c(800,301),ylim=c(0,0.01))
+# lines(800:301,c2,col=2)
+# lines(800:301,c3,col=3)
 
 
-c1  <- dLogisticGrowth2(800:301,r=sparam$r[1],a=800,b=301,m=sparam$m[1],log=F)
-c2  <- dLogisticGrowth2(800:301,r=sparam$r[5483],a=800,b=301,m=sparam$m[5483],log=F)
-c3  <- dLogisticGrowth2(800:301,r=sparam$r[3000],a=800,b=301,m=sparam$m[3000],log=F)
-plot(800:301,c1,type='l',xlim=c(800,301),ylim=c(0,0.01))
-lines(800:301,c2,col=2)
-lines(800:301,c3,col=3)
+# Fit Logistic model
+caldates  <- replicate(500,rLogisticGrowth2(1,r=rr[1],a=800,b=301,m=mm[1]))
+probMatEqui <- time2phase(caldates,data.frame(names=letters[1:2],starts=c(800,500),ends=c(501,301)))[,2:3] |> createProbMat(timeRange=c(800,301),resolution=1)
+fit  <- logisticfit(probMatEqui,rPrior='dunif(0.001,0.03)',mPrior='dunif(1,z)')
+save(rr,mm,sparam,fit,here('new_sim','equifinality.RData'))
+
 
